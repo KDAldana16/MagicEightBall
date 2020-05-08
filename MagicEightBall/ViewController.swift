5ae22361f6c82bc5cb5665856d129064f8c5b5d3
Karla Aldana <karla.aldana@students.dominican.edu>
2020-05-08 12:49:42 -0700

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Karla Aldana on 5/8/20.
+//  Copyright © 2020 Karla Aldana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
